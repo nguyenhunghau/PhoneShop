8a403f92d44207527fa04d72a0263dd5c5b250e0
Tuong <mailto:ducnmse04199@fpt.edu.vn>
2022-01-06 20:14:48 +0700

    init project

---
+++ src/main/java/com/PhoneShop/controller/HelloController.java
@@ -0,0 +1,18 @@
+//package com.PhoneShop.controller;
+//
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import static org.springframework.http.ResponseEntity.ok;
+//
+//@RestController
+//@RequestMapping("/teaching")
+//public class HelloController {
+//
+//    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+//    public ResponseEntity<String> hello() {
+//        return ok("hello");
+//    }
+//}
