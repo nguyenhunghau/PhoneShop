05244767cbe5557f152664d920def86c418de22b
lephuquan <80085752+lephuquan@users.noreply.github.com>
2022-01-11 15:37:19 +0700

    add template

--- src/main/java/com/PhoneShop/controller/PagesController.java
+++ src/main/java/com/PhoneShop/controller/PagesController.java
@@ -15,4 +15,38 @@ public class PagesController {
     public String productDetail() {
         return "pages/product-detail.html";
     }
+
+    @GetMapping("/category")
+    public String category() {
+        return "pages/category.html";
+    }
+
+    @GetMapping("/news")
+    public String News() {
+        return "pages/news.html";
+    }
+    @GetMapping("/question-and-answer")
+    public String questionAndAnswer() {
+        return "pages/question-and-answer.html";
+    }
+
+    @GetMapping("/cart")
+    public String cart() {
+        return "pages/cart.html";
+    }
+
+    @GetMapping("/billing-information")
+    public String billingInformation() {
+        return "pages/billing-information.html";
+    }
+
+    @GetMapping("/order-success")
+    public String orderSuccess() {
+        return "pages/order-success.html";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "pages/login.html";
+    }
 }
