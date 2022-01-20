60e3f425256e6e3eb0a427100f188aa42c5ed7a3
lephuquan <80085752+lephuquan@users.noreply.github.com>
2022-01-20 19:14:53 +0700

    categoryDetail

--- src/main/java/com/PhoneShop/controller/CategoryDetailController.java
+++ src/main/java/com/PhoneShop/controller/CategoryDetailController.java
@@ -1,24 +1,24 @@
-//package com.PhoneShop.controller;
-//
-//import com.PhoneShop.service.CategoryDetailService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import static org.springframework.http.ResponseEntity.ok;
-//
-//@RestController
-//@RequestMapping("/category-detail-data")
-//public class CategoryDetailController {
-//
-//    @Autowired
-//    private CategoryDetailService categoryDetailService;
-//
-//    @RequestMapping(method = RequestMethod.GET)
-//    public ResponseEntity<?> findAll() {
-//        return ok(categoryDetailService.findAll());
-//    }
-//}
+package com.PhoneShop.controller;
+
+import com.PhoneShop.service.CategoryDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.ResponseEntity.ok;
+
+@RestController
+@RequestMapping("/category-detail-data")
+public class CategoryDetailController {
+
+    @Autowired
+    private CategoryDetailService categoryDetailService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<?> findAll() {
+        return ok(categoryDetailService.findAll());
+    }
+}
