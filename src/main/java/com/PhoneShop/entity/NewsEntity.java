95984cb8da71299265fa42fc12b2090364a0da24
Tuong <mailto:ducnmse04199@fpt.edu.vn>
2022-02-27 16:53:13 +0700

    Update entity

--- src/main/java/com/PhoneShop/entity/NewsEntity.java
+++ src/main/java/com/PhoneShop/entity/NewsEntity.java
@@ -5,10 +5,118 @@
  */
 package com.PhoneShop.entity;
 
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 /**
  *
- * @author Admin
+ * @author abc
  */
-public class NewsEntity {
+@Entity
+@Table(name = "news")
+@NamedQueries({
+    @NamedQuery(name = "NewsEntity.findAll", query = "SELECT n FROM NewsEntity n")})
+public class NewsEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "photo")
+    private String photo;
+    @Lob
+    @Column(name = "description")
+    private String description;
+    @Column(name = "created_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    public NewsEntity() {
+    }
+
+    public NewsEntity(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof NewsEntity)) {
+            return false;
+        }
+        NewsEntity other = (NewsEntity) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.PhoneShop.entity.NewsEntity[ id=" + id + " ]";
+    }
     
 }
